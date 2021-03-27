356ac55c36996c2d757f97c560876b98c8bcd66a
Yaz Burrell <yburrell@zillion.com>
2021-03-27 12:43:13 -0400

    first commit

---
+++ KoneskaProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KoneskaProject
+//
+//  Created by Yaz Burrell on 3/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
